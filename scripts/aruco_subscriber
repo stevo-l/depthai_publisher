641f4506c528ad7a615395394d992b24e360ccea
dennis_brar <shahzads.brar@gmail.com>
2022-05-06 09:10:24 +1000

    aruco

---
+++ scripts/aruco_subscriber
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from depthai_publisher import aruco_subscriber
+
+if __name__ == '__main__':
+    aruco_subscriber.main()
---
+++ src/depthai_publisher/aruco_subscriber.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import queue
+import rospy
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+
+import cv2
+import numpy as np
+
+
+class ArucoDetector():
+    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
+    aruco_params = cv2.aruco.DetectorParameters_create()
+
+    frame_sub_topic = '/depthai_node/image/compressed'
+
+    def __init__(self):
+        self.aruco_pub = rospy.Publisher(
+            '/processed_aruco/image/compressed', CompressedImage, queue_size=10)
+
+        self.br = CvBridge()
+
+        if not rospy.is_shutdown():
+            self.frame_sub = rospy.Subscriber(
+                self.frame_sub_topic, CompressedImage, self.img_callback)
+
+    def img_callback(self, msg_in):
+        try:
+            frame = self.br.compressed_imgmsg_to_cv2(msg_in)
+        except CvBridgeError as e:
+            rospy.logerr(e)
+
+        aruco = self.find_aruco(frame)
+
+        cv2.imshow('aruco', aruco)
+        cv2.waitKey(1)
+
+    def find_aruco(self, frame):
+        (corners, ids, _) = cv2.aruco.detectMarkers(
+            frame, self.aruco_dict, parameters=self.aruco_params)
+
+        if len(corners) > 0:
+            ids = ids.flatten()
+
+            for (marker_corner, marker_ID) in zip(corners, ids):
+                corners = marker_corner.reshape((4, 2))
+                (top_left, top_right, bottom_right, bottom_left) = corners
+
+                top_right = (int(top_right[0]), int(top_right[1]))
+                bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+                bottom_left = (int(bottom_left[0]), int(bottom_left[1]))
+                top_left = (int(top_left[0]), int(top_left[1]))
+
+                cv2.line(frame, top_left, top_right, (0, 255, 0), 2)
+                cv2.line(frame, top_right, bottom_right, (0, 255, 0), 2)
+                cv2.line(frame, bottom_right, bottom_left, (0, 255, 0), 2)
+                cv2.line(frame, bottom_left, top_left, (0, 255, 0), 2)
+
+                rospy.loginfo("Aruco detected, ID: {}".format(marker_ID))
+
+                cv2.putText(frame, str(
+                    marker_ID), (top_left[0], top_right[1] - 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 2)
+
+        return frame
+
+    def publish_to_ros(self, frame):
+        msg_out = CompressedImage()
+        msg_out.header.stamp = rospy.Time.now()
+        msg_out.format = "jpeg"
+        msg_out.data = np.array(cv2.imencode('.jpg', frame)[1]).tostring()
+
+        self.pub_image.publish(msg_out)
+
+
+def main():
+    rospy.init_node('EGB349_vision', anonymous=True)
+    rospy.loginfo("Processing images...")
+
+    aruco_detect = ArucoDetector()
+
+    rospy.spin()
